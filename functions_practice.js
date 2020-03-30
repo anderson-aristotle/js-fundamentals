edae6f41d3224a93c789d5e0d91214242f722385
rex@billion <anderson.aristotle@gmail.com>
2020-03-30 17:19:33 -0400

    first commit

---
+++ functions_practice.js
@@ -0,0 +1,28 @@
+(() => {
+  // 1. Create a function that console logs hello world
+
+  const f1 = () => {
+    // fill in
+  };
+  f1(); // should console log "Hello World!"
+
+  // 2. Create a function that takes a string and console logs that string
+  console.log('Goodbye World');
+
+  const f2 = inputString => {
+    // fill in
+  };
+
+  f2('Hello World!'); // should console log "Hello World!"
+  f2('GoodBye World!'); // should console log "GoodBye World!"
+  // 3. Create a function that takes an array of strings an console logs
+  const f3 = inputArray => {
+    // fill in
+  };
+  f3('Hi!', 'Bye'); // should console log "Hi!", and then console log "Bye"
+  // 4. Create a function that takes an object and console log's its message value
+  const f4 = inputObject => {
+    // fill in
+  };
+  f4({ message: 'Hello!' }); // should console log "Hello!"
+})();
---
+++ sanity.js
@@ -0,0 +1,3 @@
+console.log("!!!!!");
+
+const a = "2222";
