ba47ff64a5ebf8ffda6f950089350967df1a3627
rex@billion <anderson.aristotle@gmail.com>
2020-04-02 12:09:26 -0400

    initial commit

--- functions_practice.js
+++ functions_practice.js
@@ -3,23 +3,25 @@
 
   const f1 = () => {
     // fill in
+    console.log('Hello World');
   };
+
   f1(); // should console log "Hello World!"
 
   // 2. Create a function that takes a string and console logs that string
-  console.log('Goodbye World');
 
   const f2 = inputString => {
-    // fill in
+    console.log(inputString + ' ' + 'World!');
   };
+  f2('Hello'); // should console log "Hello World!"
+  f2('GoodBye'); // should console log "GoodBye World!"
 
-  f2('Hello World!'); // should console log "Hello World!"
-  f2('GoodBye World!'); // should console log "GoodBye World!"
   // 3. Create a function that takes an array of strings an console logs
   const f3 = inputArray => {
-    // fill in
-  };
-  f3('Hi!', 'Bye'); // should console log "Hi!", and then console log "Bye"
+    console.log('Hi!');
+    console.log('Bye!');
+  }; // should console log "Hi!", and then console log "Bye"
+  f3(console.log());
   // 4. Create a function that takes an object and console log's its message value
   const f4 = inputObject => {
     // fill in
